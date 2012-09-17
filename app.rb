9b6f3f40c91d2a13bfc890bb6a72f6e35873cf17
markkleeb <kleeb930@gmail.com>
2012-09-17 17:25:55 -0400

    blob

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ class Car
   include DataMapper::Resource
   
   property :id, Serial, :key => true
-  property :rfid, String  #RFID number
+  property :rfid, Blob  #RFID number
   property :carname, String
   property :name, String
 
@@ -124,7 +124,7 @@ post '/newcar' do
   car = Car.new
   
   car.id = params[:id]
-  car.rfid = params[:rfid].to_s
+  car.rfid = params[:rfid]
   car.carname = params[:carname]
   car.name = params[:name]
   
